e828f55d155de420002941471e1f6e650f6378f6
penghaitao918 <penghaitao918@163.com>
2015-08-10 10:44:42 +0800

    # Version 1.2.0 内存处理 # push by penhaitao # 2015/08/09 16：07

--- app/src/main/java/com/android/decipherstranger/Network/NetConnect.java
+++ app/src/main/java/com/android/decipherstranger/Network/NetConnect.java
@@ -11,7 +11,9 @@ import java.net.Socket;
 public class NetConnect extends Thread {
 
     /*  information about Server, ip address and portal number  */
-    //private String HostIp = "192.168.32.128";
+    /*工程实验室测试*/
+    //  private String HostIp = "10.33.1.28";
+    /*运行服务器*/
     private String HostIp = "10.163.16.65";
 
     private int HostPort = 8283;
